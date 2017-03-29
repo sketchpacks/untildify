f916aa71f6e207870b4b9bb1d7681ea928847ca2
Adam Kirkwood <hello@adamkirkwood.com>
2017-03-29 07:11:58 -0700

    :bug: Fix webpack compilation error

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
+
 const os = require('os');
+
 const home = os.homedir();
 
-module.exports = str => {
+function expandPath(str) {
 	if (typeof str !== 'string') {
 		throw new TypeError(`Expected a string, got ${typeof str}`);
 	}
 
 	return home ? str.replace(/^~($|\/|\\)/, `${home}$1`) : str;
-};
+}
+
+module.exports = expandPath;
