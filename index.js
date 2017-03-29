828f57216981c188e5262de0f76221c39f49bf96
Adam Kirkwood <hello@adamkirkwood.com>
2017-03-29 07:32:22 -0700

    :bug: Fix string interpolation

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const home = os.homedir();
 
 function expandPath(str) {
 	if (typeof str !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof str}`);
+		throw new TypeError(['Expected a string, got ', typeof str].join(''));
 	}
 
-	return home ? str.replace(/^~($|\/|\\)/, `${home}$1`) : str;
+	return home ? str.replace(/^~($|\/|\\)/, [home,'$1'].join('')) : str;
 }
 
 module.exports = expandPath;
